8fe2a9e603890d6552ee18264e80055f2a628f08
Rizart Dona <rizart.dona@gmail.com>
2020-10-26 16:48:48 +0100

    localpath env variable renaming

--- fts_datalake_test.sh
+++ fts_datalake_test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export LOCALPATH="/tmp/ridona/temp_files_fts"
+export FTS_LOCALPATH="/tmp/ridona/temp_files_fts"
 
 kdestroy # ad-hoc solution for account:ridona@eulake
 /usr/bin/voms-proxy-init -voms escape
 export X509_USER_PROXY=/tmp/x509up_u127450
 
-mkdir -p $LOCALPATH
+mkdir -p $FTS_LOCALPATH
 
 python fts_datalake_test.py -i conf/datalake.json --cleanup --exit
 python fts_datalake_test.py -i conf/datalake.json
